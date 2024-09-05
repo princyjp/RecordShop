6f40abf81d3e0469c5ad81e4a4f97d5f2d74e3b1
princyjp <princy.philip@outlook.com>
2024-09-05 15:18:43 +0100

    feat:controller:add endpoint getAllAlbums()

--- src/main/java/com/northcoders/RecordShopAPI/controller/RecordShopController.java
+++ src/main/java/com/northcoders/RecordShopAPI/controller/RecordShopController.java
@@ -1,4 +1,26 @@
 package com.northcoders.RecordShopAPI.controller;
 
+import com.northcoders.RecordShopAPI.model.Album;
+import com.northcoders.RecordShopAPI.service.RecordShopService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/recordshop")
 public class RecordShopController {
+
+    @Autowired
+    RecordShopService recordShopService;
+
+    @GetMapping
+    public ResponseEntity<List<Album>> getAllAlbums(){
+        return new ResponseEntity<>(recordShopService.getAllAlbums(), HttpStatus.OK);
+    }
+
 }
