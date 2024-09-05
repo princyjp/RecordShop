f5cadd5f62e28d62b17677b928abede079c8d2ce
princyjp <princy.philip@outlook.com>
2024-09-05 15:17:48 +0100

    test:Controller: getAllAlbums()

---
+++ src/test/java/com/northcoders/RecordShopAPI/controller/RecordShopControllerTest.java
@@ -0,0 +1,82 @@
+package com.northcoders.RecordShopAPI.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.northcoders.RecordShopAPI.model.Album;
+import com.northcoders.RecordShopAPI.model.Artist;
+import com.northcoders.RecordShopAPI.model.Genre;
+import com.northcoders.RecordShopAPI.service.RecordShopService;
+import com.northcoders.RecordShopAPI.service.RecordShopServiceImpl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.math.BigDecimal;
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class RecordShopControllerTest {
+
+    @Mock
+    private RecordShopServiceImpl mockrecordShopServiceImpl;
+
+    @InjectMocks
+    private RecordShopController recordShopController;
+    @Autowired
+    private MockMvc mockMvcController;
+    private ObjectMapper mapper;
+    private static List<Album> albums;
+
+    @BeforeEach
+    public void setup(){
+        mockMvcController = MockMvcBuilders.standaloneSetup(recordShopController).build();
+        mapper = new ObjectMapper();
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        albums = new ArrayList<>();
+        albums.add(Album.builder()
+                .title("Black Sbbath")
+                .artist("N.I.B")
+                .genre(Genre.METAL)
+                .release_year(Year.of(2009))
+                .stock(4)
+                .price(BigDecimal.valueOf(23.3)).build());
+        albums.add(Album.builder()
+                .title("Waiting for a train")
+                .artist("Jimmie Rodgers")
+                .genre(Genre.COUNTRY)
+                .release_year(Year.of(2000))
+                .stock(14)
+                .price(BigDecimal.valueOf(33.983)).build());
+    }
+
+    @Test
+    void getAllAlbums() throws Exception{
+        when(mockrecordShopServiceImpl.getAllAlbums()).thenReturn(albums);
+
+ this.mockMvcController.perform(
+         MockMvcRequestBuilders.get("/api/v1/recordshop"))
+                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].title").value("Black Sbbath"))
+                 .andExpect(MockMvcResultMatchers.jsonPath("$[1].title").value("Waiting for a train"))
+                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].stock").value(4))
+                 .andExpect(MockMvcResultMatchers.jsonPath("$[1].stock").value(14));
+
+
+    }
+}
