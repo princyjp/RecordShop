b3d8d91257fe1c98870b6927381c30f9805d44a8
princyjp <princy.philip@outlook.com>
2024-09-05 15:48:29 +0100

    feat:service:implemetn getAllAlbums()

--- src/main/java/com/northcoders/RecordShopAPI/service/RecordShopServiceImpl.java
+++ src/main/java/com/northcoders/RecordShopAPI/service/RecordShopServiceImpl.java
@@ -1,12 +1,31 @@
 package com.northcoders.RecordShopAPI.service;
 
+import com.northcoders.RecordShopAPI.model.Album;
 import com.northcoders.RecordShopAPI.repository.AlbumRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
-public class RecordShopImpl implements RecordShop{
+public class RecordShopServiceImpl implements RecordShopService {
 
     @Autowired
     AlbumRepository albumRepository;
+
+    @Override
+    public List<Album> getAllAlbums() {
+        List<Album> albums = new ArrayList<>();
+        albumRepository.findAll().forEach(albums::add);
+        return albums;
+    }
+
+    @Override
+    public Album getALbumById(Long id) {
+        if(albumRepository.findById(id).isPresent())
+        return albumRepository.findById(id).get();
+
+        return null;
+    }
 }
---
+++ src/test/java/com/northcoders/RecordShopAPI/service/RecordShopServiceImplTest.java
@@ -0,0 +1,16 @@
+package com.northcoders.RecordShopAPI.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecordShopServiceImplTest {
+
+    @Test
+    void getAllAlbums() {
+    }
+
+    @Test
+    void getALbumById() {
+    }
+}
