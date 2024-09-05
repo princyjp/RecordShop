fe2dac377e49c4fa3c78e65fc8b0651566436e56
princyjp <princy.philip@outlook.com>
2024-09-05 10:06:47 +0100

    feat: added postgresql and H2 gitignore files

---
+++ src/test/java/com/northcoders/RecordShopAPI/RecordShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcoders.RecordShopAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
