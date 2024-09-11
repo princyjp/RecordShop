bd5c4dd530e40f9ddce513c14f2d7b17ea9afda3
princyjp <princy.philip@outlook.com>
2024-09-11 12:06:59 +0100

    test:service: add test for post method

--- src/test/java/com/northcoders/RecordShopAPI/service/AlbumsServiceImplTest.java
+++ src/test/java/com/northcoders/RecordShopAPI/service/AlbumsServiceImplTest.java
@@ -1,9 +1,15 @@
 package com.northcoders.RecordShopAPI.service;
 
+import com.northcoders.RecordShopAPI.DTO.AlbumDTO;
+import com.northcoders.RecordShopAPI.DTO.ArtistDTO;
+import com.northcoders.RecordShopAPI.exception.DuplicateEntryException;
+import com.northcoders.RecordShopAPI.exception.ItemNotFoundException;
 import com.northcoders.RecordShopAPI.model.Album;
 import com.northcoders.RecordShopAPI.model.Artist;
 import com.northcoders.RecordShopAPI.model.Genre;
 import com.northcoders.RecordShopAPI.repository.AlbumRepository;
+import com.northcoders.RecordShopAPI.repository.ArtistRepository;
+import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -16,17 +22,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @DataJpaTest
 class AlbumsServiceImplTest {
     @Mock
     AlbumRepository mockAlbumRepository;
 
+    @Mock
+    ArtistRepository mockArtistRepository;
+
     @InjectMocks
     AlbumsServiceImpl recordShopServiceImpl;
     private static List<Album> albums;
+    private static List<AlbumDTO> albumDTO;
 
     @BeforeAll
     static void beforeAll() {
@@ -35,36 +47,71 @@ class AlbumsServiceImplTest {
                 .title("Black Sbbath")
                 .artist(Artist.builder().artist_id(1L).name("N.I.B").build())
                 .genre(Genre.METAL)
-                .release_year(Year.of(2009))
+                .releaseYear(Year.of(2009))
                 .stock(4)
                 .price(BigDecimal.valueOf(23.3)).build());
         albums.add(Album.builder()
                 .title("Waiting for a train")
                 .artist(Artist.builder().artist_id(2L).name("Jimmie Rodgers").build())
                 .genre(Genre.COUNTRY)
-                .release_year(Year.of(2000))
+                .releaseYear(Year.of(2000))
                 .stock(14)
                 .price(BigDecimal.valueOf(33.983)).build());
+        albumDTO= new ArrayList<>();
+        albums.forEach(album -> {
+            AlbumDTO dto = new AlbumDTO();
+            dto.setId(album.getAlbumId());
+            dto.setTitle(album.getTitle());
+            dto.setReleaseYear(album.getReleaseYear());
+            dto.setGenre(album.getGenre());
+            dto.setPrice(album.getPrice());
+            dto.setStock(album.getStock());
+            dto.setArtist(new ArtistDTO(album.getArtist().getName()));
+            albumDTO.add(dto);
+        });
     }
 
     @Test
     void getAllAlbumsTest() {
         when(mockAlbumRepository.findAll()).thenReturn(albums);
-        List<Album> result = recordShopServiceImpl.getAllAlbums();
+        List<AlbumDTO> result = recordShopServiceImpl.getAllAlbums();
         assertThat(result).hasSize(2);
-        assertThat(result).isEqualTo(albums);
+        assertThat(result.get(0).getTitle()).isEqualTo(albums.get(0).getTitle());
+        assertThat(result.get(0).getId()).isEqualTo(albums.get(0).getAlbumId());
+        assertThat(result.get(0).getReleaseYear()).isEqualTo(albums.get(0).getReleaseYear());
     }
 
     @Test
     void getAlbumByIdTest() {
         when(mockAlbumRepository.findById(2L)).thenReturn(Optional.ofNullable(albums.get(1)));
-        var result = recordShopServiceImpl.getALbumById(2L);
-        assertThat(result).isEqualTo(albums.get(1));
+        var result = recordShopServiceImpl.getAlbumById(2L);
+        assertThat(result.getId()).isEqualTo(albums.get(1).getAlbumId());
+        assertThat(result.getTitle()).isEqualTo(albums.get(1).getTitle());
     }
     @Test
-    void saveOrUpdateAlbumTest() {
+    void getAlbumByIdTest_AlbumNotFoundException() {
+        when(mockAlbumRepository.findById(3L)).thenReturn(Optional.empty());
+       assertThrows(ItemNotFoundException.class, ()-> recordShopServiceImpl.getAlbumById(3L));
+
+    }
+    @Test
+    void saveAlbumTest() {
+        when(mockArtistRepository.save(albums.get(1).getArtist())).thenReturn(albums.get(1).getArtist());
         when(mockAlbumRepository.save(albums.get(1))).thenReturn(albums.get(1));
         var result = recordShopServiceImpl.saveOrUpdateAlbum(albums.get(1));
-        assertThat(result).isEqualTo(albums.get(1));
+        assertThat(result.getId()).isEqualTo(albums.get(1).getAlbumId());
+        assertThat(result.getTitle()).isEqualTo(albums.get(1).getTitle());
+    }
+    @Test
+    void saveAlbumTest_DuplicateEntryException() {
+        when(mockArtistRepository.save(albums.get(1).getArtist())).thenReturn(albums.get(1).getArtist());
+        when(mockAlbumRepository.save(albums.get(1))).thenThrow( new DuplicateEntryException("Album named Waiting for a train already exists."));
+
+        DuplicateEntryException exception = assertThrows(DuplicateEntryException.class, ()-> {
+            recordShopServiceImpl.saveOrUpdateAlbum(albums.get(1));
+        });
+        assertEquals("Album named Waiting for a train already exists.",exception.getMessage());
+        verify(mockAlbumRepository,times(1)).save(albums.get(1));
+
     }
 }
